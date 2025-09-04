68c8c1b39ae0ab9d720b3cf60d114b18bc603937
Satyavrat Dubey <166104655+Satyavrat-Dubey@users.noreply.github.com>
2025-09-04 19:17:24 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,8 @@
+
+var typed = new Typed(".text", {
+    strings: ["Backend Developer . . .","Java Developer . . .","Frontend Developer . . ." , "Programmer . . ."],
+    typeSpeed: 70,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+})
